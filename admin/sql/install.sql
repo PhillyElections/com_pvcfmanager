866548b048400f2f546292a1f9a01d9d66e0c800
matthew murphy <mattyhead@gmail.com>
2019-04-15 15:37:23 -0400

    schema

--- admin/sql/install.sql
+++ admin/sql/install.sql
@@ -7,9 +7,63 @@ SET @db   = DATABASE();
 
 /* ==================== tables ==================== */
 
-CREATE TABLE IF NOT EXISTS `#__pvcfmanager` (
+CREATE TABLE IF NOT EXISTS `#__pv_cf_online_reports` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `field` varchar(100) NOT NULL DEFAULT '',
+  `filername` varchar(255) NOT NULL DEFAULT '',
+  `reporturl` varchar(255) NOT NULL DEFAULT '',
+  `year` smallint(4) NOT NULL DEFAULT '',
+  `class` varchar(255) NOT NULL DEFAULT '',
+  `cycle` varchar(255) NOT NULL DEFAULT '',
+  `display` varchar(255) NOT NULL DEFAULT '',
+  `ordinal` smallint(4) NOT NULL DEFAULT '',
+  `reporttype` varchar(255) NOT NULL DEFAULT '',
+  `reportid` varchar(255) NOT NULL DEFAULT '',
+  `ordinal` smallint(4) NOT NULL DEFAULT '',
+  `published` tinyint(1) unsigned NOT NULL DEFAULT 0,
+  `checked_out` int(10) unsigned NOT NULL DEFAULT 0,
+  `checked_out_time` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `created` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `updated` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `#__pv_cf_paper_reports` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `filername` varchar(255) NOT NULL DEFAULT '',
+  `reporturl` varchar(255) NOT NULL DEFAULT '',
+  `year` smallint(4) NOT NULL DEFAULT '',
+  `class` varchar(255) NOT NULL DEFAULT '',
+  `cycle` varchar(255) NOT NULL DEFAULT '',
+  `display` varchar(255) NOT NULL DEFAULT '',
+  `ordinal` smallint(4) NOT NULL DEFAULT '',
+  `published` tinyint(1) unsigned NOT NULL DEFAULT 0,
+  `checked_out` int(10) unsigned NOT NULL DEFAULT 0,
+  `checked_out_time` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `created` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `updated` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `#__pv_cf_online_maps` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `class` varchar(255) NOT NULL DEFAULT '',
+  `entity` varchar(255) NOT NULL DEFAULT '',
+  `display` varchar(255) NOT NULL DEFAULT '',
+  `year` smallint(4) NOT NULL DEFAULT '',
+  `published` tinyint(1) unsigned NOT NULL DEFAULT 0,
+  `checked_out` int(10) unsigned NOT NULL DEFAULT 0,
+  `checked_out_time` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `created` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `updated` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `#__pv_cf_file_maps` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `class` varchar(255) NOT NULL DEFAULT '',
+  `entity` varchar(255) NOT NULL DEFAULT '',
+  `display` varchar(255) NOT NULL DEFAULT '',
+  `year` smallint(4) NOT NULL DEFAULT '',
   `published` tinyint(1) unsigned NOT NULL DEFAULT 0,
   `checked_out` int(10) unsigned NOT NULL DEFAULT 0,
   `checked_out_time` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
--- admin/sql/uninstall.sql
+++ admin/sql/uninstall.sql
@@ -1,2 +1,6 @@
 DROP TABLE 
-`#__pvcfmanager`;
+`#__pv_cf_online_reports`,
+`#__pv_cf_paper_reports`
+`#__pv_cf_online_maps`
+`#__pv_cf_paper_maps`
+;
