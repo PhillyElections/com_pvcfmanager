d276d1ed684d0b2cff79446e5f74e8cef8c0a2db
matthew murphy <mattyhead@gmail.com>
2019-04-24 10:08:43 -0400

    ...

--- admin/views/report/tmpl/default.php
+++ admin/views/report/tmpl/default.php
@@ -77,7 +77,7 @@ var_dump($this->classes);
                     </label>
                 </td>
                 <td>
-                    <?=JHTML::_('select.genericlist', PVCombo::getsFromObject($sources, 'id', 'name', 'Select a source'), 'source', '', 'idx', 'value', ($row->source ? $row->source : ''), 'source');?>
+                    <?=JHTML::_('select.genericlist', PVCombo::getsFromArray($sources, 'id', 'name', 'Select a source'), 'source', '', 'idx', 'value', ($row->source ? $row->source : ''), 'source');?>
                 </td>
             </tr>
             <tr>
